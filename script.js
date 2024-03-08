d7e85c66aed40ce25251d8ffa22335bb26a98585
awanishkrai <151035327+awanishkrai@users.noreply.github.com>
2024-03-09 03:29:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function test() {
+  var name = document.getElementById("sname").value;
+  var state = document.getElementById("sstate").value;
+  var caste = document.getElementById("scaste").value;
+  var gender = document.getElementById("sgender").value;
+  var age = document.getElementById("sage").value;
+  var sclass = document.getElementById("sclass").value;
+  var reason = document.getElementById("sreason").value;
+  alert("This form is successfully submitted");
+}
